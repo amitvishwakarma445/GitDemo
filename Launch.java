1a26dd68850ea4bb4b73e05104662a8f0b11ecd6
amitvishwakarma445 <amit.vishwakarma.ds@gmail.com>
2025-07-11 10:43:10 +0530

    modified after cloning

--- Launch.java
+++ Launch.java
@@ -2,4 +2,11 @@ class Launch{
 	public static void main(String[] aaa){
 		System.out.println("demonstrate demo of Git");
 	}
+
+	public static void fun(){
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println(sum);
+	}
 }
